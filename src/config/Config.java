2013d36802dfa7a338d4f8304f0c8ca26cae585c
joaquingatica@gmail.com <joaquingatica@gmail.com>
2012-04-21 17:25:02 -0300

    Branch adjustments

---
+++ src/config/Config.java
@@ -0,0 +1,11 @@
+package config;
+
+public class Config {
+	
+	private static String GOOGLE_MAPS_API_KEY = "AIzaSyAK9hXpQhpoPoF9hSEHIBcqACmJcbHtW8c";
+	
+	public String getGoogleMapsApiKey() {
+		return GOOGLE_MAPS_API_KEY;
+	}
+	
+}
