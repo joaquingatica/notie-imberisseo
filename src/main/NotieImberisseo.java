56f64a881d9cea9a841261b22abb00e4ffedbcca
joaquingatica@gmail.com <joaquingatica@gmail.com>
2012-04-28 17:36:07 -0300

    Added GNU GPL licence and copyright notices.

--- src/main/NotieImberisseo.java
+++ src/main/NotieImberisseo.java
@@ -1,3 +1,30 @@
+/**
+ * 
+ * Notië Imberissëo
+ * 
+ * Copyright 2012 Joaquín Gatica (Erutulco Eruntano)
+ * 
+ * Contact:
+ * 		Twitter: <http://twitter.com/joaquingatica>
+ * 		Email: <erutulco@quenya101.com>
+ * 
+ * This file is part of "Notië Imberissëo".
+ * 
+ * "Notië Imberissëo" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * "Notië Imberissëo" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with "Notië Imberissëo".  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package main;
 
 import java.awt.EventQueue;
