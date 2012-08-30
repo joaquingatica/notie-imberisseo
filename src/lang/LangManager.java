316644425f1437c4c7f64eba78225073360d9938
joaquingatica <joaquingatica@gmail.com>
2012-08-29 23:42:17 -0300

    Removed Tengwar Beta for release.

--- src/lang/LangManager.java
+++ src/lang/LangManager.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 
 public class LangManager {
 	
-	private static final String[] languages = {"Quenya", "QuenyaTengwar", "English", "Espanol", "Deutsch"};
-	private static final String[] languages_short = {"qya", "qyt", "eng", "esp", "deu"};
-	private static final String[] languages_printable = {"Quenya", "Quenya Tengwar (Beta)", "English", "Español", "Deutsch"};
+	private static final String[] languages = {"Quenya"/*, "QuenyaTengwar"*/, "English", "Espanol", "Deutsch"};
+	private static final String[] languages_short = {"qya"/*, "qyt"*/, "eng", "esp", "deu"};
+	private static final String[] languages_printable = {"Quenya"/*, "Quenya Tengwar (Beta)"*/, "English", "Español", "Deutsch"};
 	
 	private HashMap<String,Lang> langs;
 	private Lang definedLang;
